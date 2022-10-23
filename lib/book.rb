506379e1030da8d6c410f02f2fb2a449a07a8e88
nkirotemoreen <nkirotemoreen70@gmail.com>
2022-10-23 05:00:58 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,12 @@
 class Book
-    attr_accessor :author, :page_count, :genre, :turn_page
+    attr_accessor :author, :page_count, :genre, :turn_page #this values do change
 
-    attr_reader :title
+    attr_reader :title #used reader because the value doesnt chnage .its fixed
      
     def initialize(title)
         @title = title
     end
+    #method
     def turn_page
         puts "Flipping the page...wow, you read fast!"
     end
