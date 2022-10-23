dcc104e3637ec205073f78af691c510770556ff3
nkirotemoreen <nkirotemoreen70@gmail.com>
2022-10-23 04:58:24 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
+class Book
+    attr_accessor :author, :page_count, :genre, :turn_page
+
+    attr_reader :title
+     
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+    
+end
+
+bookdetails = Book.new("Alchemist")
+puts bookdetails.author ="paulo"
+
+puts bookdetails.title
+puts bookdetails.page_count ="272"
+puts bookdetails.genre = "nature"
+puts bookdetails.turn_page
+
+
+
 
 
